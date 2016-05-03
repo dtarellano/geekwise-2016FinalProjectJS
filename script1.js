9a47949971e7e2f5d86061846a83075ca7c4a319
arellanodavid <dtarellano1@gmail.com>
2016-05-02 20:25:34 -0700

    day 1

---
+++ script1.js
@@ -0,0 +1,8 @@
+var wordInput = prompt("Please give a word for game to begin") ;
+
+function setup() {
+
+    availableLetters = "abcdefghijklmnopqrstuvwxyz";
+    lives = 5;
+    words = wordInput;
+};
