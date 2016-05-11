fe3a0814b852cedff0d0ef251a7cd4d4705a9ed4
arellanodavid <dtarellano1@gmail.com>
2016-05-11 13:19:13 -0700

    switched to music box
    due to mental frustration, I went back to the music box from hangman

--- script1.js
+++ script1.js
@@ -1,8 +1,35 @@
-var wordInput = prompt("Please give a word for game to begin") ;
-
-function setup() {
-
-    availableLetters = "abcdefghijklmnopqrstuvwxyz";
-    lives = 5;
-    words = wordInput;
-};
+var cNote = document.getElementById('cAudio');
+    document.querySelector("#c").addEventListener("mouseover", function(){
+        cNote.currentTime= 0;
+        cNote.play()
+});
+var aNote = document.getElementById('aAudio');
+    document.querySelector("#a").addEventListener("mouseover", function(){
+        aNote.currentTime= 0;
+        aNote.play()
+});
+var bNote = document.getElementById('bAudio');
+    document.querySelector("#b").addEventListener("mouseover", function(){
+        bNote.currentTime= 0;
+        bNote.play()
+});
+var dNote = document.getElementById('dAudio');
+    document.querySelector("#d").addEventListener("mouseover", function(){
+        dNote.currentTime= 0;
+        dNote.play()
+});
+var eNote = document.getElementById('eAudio');
+    document.querySelector("#e").addEventListener("mouseover", function(){
+        eNote.currentTime= 0;
+        eNote.play()
+});
+var fNote = document.getElementById('fAudio');
+    document.querySelector("#f").addEventListener("mouseover", function(){
+        fNote.currentTime= 0;
+        fNote.play()
+});
+var gNote = document.getElementById('gAudio');
+    document.querySelector("#g").addEventListener("mouseover", function(){
+        gNote.currentTime= 0;
+        gNote.play()
+});
